c155b87f71c5f5517d83001a311c2f4408235b88
Manthankumar Satani <satanimanthan@gmail.com>
2020-06-24 13:25:54 +0530

    new: account short info

---
+++ components/lego/account/account-banner.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="row ps-y-32 ps-x-16 account-banner">
+    <div
+      class="col-12 col-lg d-flex flex-column flex-md-row justify-content-center justify-content-lg-start"
+    >
+      <svg-sprite-icon name="profile" class="profile-pic align-self-center" />
+      <div
+        class="d-flex flex-column align-self-center ps-l-md-16 text-center text-md-left ps-t-16 ps-t-md-0"
+      >
+        <div class="name ps-b-4 font-heading-small font-semibold">Account name</div>
+        <div class="address font-body-medium">0x8185ddfcf81a87977d1b16c68106c30fff9f2490</div>
+        <div class="address-short font-body-medium">0x8185d....f9f2490</div>
+      </div>
+    </div>
+    <div
+      class="col-12 col-lg d-flex left-content text-center text-sm-left justify-content-center ps-t-16 ps-t-md-32 ps-t-lg-0 justify-content-lg-end"
+    >
+      <div class="d-flex flex-column align-self-center ms-r-32 justify-content-start">
+        <div class="name ps-b-4 font-heading-small font-semibold">Total NFT</div>
+        <div class="amount font-body-medium">34</div>
+      </div>
+      <div class="d-flex flex-column align-self-center ms-r-32 justify-content-start">
+        <div class="name ps-b-4 font-heading-small font-semibold">Wallet Balance</div>
+        <div class="amount font-body-medium">$544</div>
+      </div>
+      <div class="align-self-center">
+        <button class="btn btn-light ml-auto">View Wallet</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+import Component from "nuxt-class-component";
+
+@Component({
+  props: {}
+})
+export default class AccountBanner extends Vue {
+  mounted() {}
+}
+</script>
+
+<style lang="scss" scoped="true">
+@import "~assets/css/theme/_theme";
+.account-banner {
+  background-color: primary-color("600");
+  color: light-color("700");
+
+  .profile-pic {
+    min-width: 64px !important;
+    min-height: 64px !important;
+  }
+}
+.address-short {
+  display: none;
+}
+.btn {
+  min-width: fit-content;
+}
+
+@media (max-width: 400px) {
+  .address {
+    display: none;
+  }
+  .address-short {
+    display: block;
+  }
+  .left-content {
+    flex-direction: column;
+  }
+  .ms-r-32 {
+    margin-right: 0px !important;
+  }
+  .justify-content-start {
+    justify-content: center !important;
+  }
+  .amount {
+    // padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+}
+</style>
