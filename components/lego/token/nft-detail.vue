0d92a8f5fd1f75f76600b9860a247c93af996509
ankitboghra <ankitboghra@gmail.com>
2020-11-13 18:28:41 +0530

    chg: linting

--- components/lego/token/nft-detail.vue
+++ components/lego/token/nft-detail.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div class="container-fluid ps-y-16" v-if="token.token_id && !isLoadingDetails">
+    <div
+      class="container-fluid ps-y-16"
+      v-if="token.token_id && !isLoadingDetails"
+    >
       <div class="row ps-y-16 ps-x-md-16">
         <div class="col-md-7 d-flex">
           <token-short-info
@@ -15,7 +18,7 @@
         <div class="col-md-8">
           <div
             class="feature-image d-flex d-lg-flex justify-content-center"
-            v-bind:style="{background: bg}"
+            v-bind:style="{ background: bg }"
           >
             <img
               class="asset-img align-self-center"
@@ -24,44 +27,51 @@
               @load="onImageLoad"
             />
           </div>
-          <div class="feature-info mobile d-flex d-lg-none flex-column ps-16 ps-lg-40 ms-y-16">
-            <h2>{{token.description}}</h2>
-            <h3 class="font-heading-medium font-semibold">About {{token.name}}</h3>
+          <div
+            class="feature-info mobile d-flex d-lg-none flex-column ps-16 ps-lg-40 ms-y-16"
+          >
+            <h2>{{ token.description }}</h2>
+            <h3 class="font-heading-medium font-semibold">
+              About {{ token.name }}
+            </h3>
             <p
               class="font-body-medium"
-              :class="{'show-less': showMore, 'show-more': !showMore}"
+              :class="{ 'show-less': showMore, 'show-more': !showMore }"
               v-if="token.description"
             >
-              {{token.description}}
+              {{ token.description }}
               <span class="dots">...</span>
-              <span class="more">{{token.description}}</span>
+              <span class="more">{{ token.description }}</span>
               <a
                 class="font-body-small d-flex ps-t-8 font-medium"
                 href="#more-info"
                 v-if="!showMore"
                 @click.prevent="showMore = true"
-              >More info</a>
+                >More info</a
+              >
               <a
                 class="font-body-small d-flex ps-t-8 font-medium"
                 href="#more-info"
                 v-if="showMore"
                 @click.prevent="showMore = false"
-              >Show less</a>
+                >Show less</a
+              >
             </p>
           </div>
           <div class="d-flex flex-column ps-y-16 ps-y-md-32" v-if="category">
             <h3 class="font-heading-medium font-semibold category">
-              About {{category.name}}
+              About {{ category.name }}
               <a
                 class="ps-l-12"
                 :href="category.url"
                 target="_blank"
                 rel="noopener noreferrer"
-              >Visit Website</a>
+                >Visit Website</a
+              >
 
               <span
                 class="float-right cursor-pointer right-arrow"
-                :class="{'down-icon':showCategoryInfo}"
+                :class="{ 'down-icon': showCategoryInfo }"
                 @click="showCategoryInfo = !showCategoryInfo"
                 v-if="category.description"
               >
@@ -71,54 +81,63 @@
             <p
               class="font-body-medium ps-t-20"
               v-if="showCategoryInfo && category.description"
-            >{{category.description}}</p>
+            >
+              {{ category.description }}
+            </p>
           </div>
-          <div class="d-flex flex-column ps-y-16 ps-y-md-32" v-if="token.properties">
+          <div
+            class="d-flex flex-column ps-y-16 ps-y-md-32"
+            v-if="token.properties"
+          >
             <h3 class="font-heading-medium font-semibold category">
               Properties
               <span
                 class="float-right cursor-pointer right-arrow"
-                :class="{'down-icon':showProperties}"
+                :class="{ 'down-icon': showProperties }"
                 @click="showProperties = !showProperties"
               >
                 <svg-sprite-icon name="right-arrow" />
               </span>
             </h3>
-            <p class="font-body-medium ps-t-20" v-if="showProperties">{{token.properties}}</p>
+            <p class="font-body-medium ps-t-20" v-if="showProperties">
+              {{ token.properties }}
+            </p>
           </div>
         </div>
         <div class="col-md-4 d-none d-lg-flex">
           <div class="feature-info d-flex flex-column ps-16 ps-lg-40">
-            <h3 class="font-heading-medium font-semibold">About {{token.name}}</h3>
+            <h3 class="font-heading-medium font-semibold">
+              About {{ token.name }}
+            </h3>
             <p
               class="font-body-medium"
-              :class="{'show-less': showMore, 'show-more': !showMore}"
+              :class="{ 'show-less': showMore, 'show-more': !showMore }"
               v-if="token.description"
             >
-              {{token.description}}
+              {{ token.description }}
               <span class="dots">...</span>
-              <span class="more">{{token.description}}</span>
+              <span class="more">{{ token.description }}</span>
               <a
                 class="font-body-small d-flex ps-t-8 font-medium"
                 href="#more-info"
                 v-if="!showMore"
                 @click.prevent="showMore = true"
-              >More info</a>
+                >More info</a
+              >
               <a
                 class="font-body-small d-flex ps-t-8 font-medium"
                 href="#more-info"
                 v-if="showMore"
                 @click.prevent="showMore = false"
-              >Show less</a>
+                >Show less</a
+              >
             </p>
           </div>
         </div>
       </div>
     </div>
 
-    <div
-      class="row ps-x-16 ps-y-120 d-flex justify-content-center text-center"
-    >
+    <div class="row ps-x-16 ps-y-120 d-flex justify-content-center text-center">
       <button-loader
         class="mx-auto"
         :loading="isLoadingDetails"
@@ -229,7 +248,7 @@ export default class NftDetail extends Vue {
     this.isLoadingDetails = true;
     try {
       let response = await getAxios().get(
-          `tokens/balance?userId=${this.user.id}&chainId=${this.chainId}`
+        `tokens/balance?userId=${this.user.id}&chainId=${this.chainId}`
       );
       if (response.status === 200 && response.data.data) {
         let allTokens = [];
@@ -241,7 +260,7 @@ export default class NftDetail extends Vue {
           if (token.token_id == this.tokenId) {
             this.token = token;
           }
-        })
+        });
       }
     } catch (error) {
       console.log(error);
