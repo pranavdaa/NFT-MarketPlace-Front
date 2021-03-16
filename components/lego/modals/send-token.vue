8b6418660e24ff5d08b782fa11a70bd727ab59e4
Nareshkumar JS <iamnarey@gmail.com>
2021-03-16 22:14:55 +0530

    chg: refresh nft list using timeout

--- components/lego/modals/send-token.vue
+++ components/lego/modals/send-token.vue
@@ -385,7 +385,10 @@ export default class SendToken extends Vue {
             });
           if (erc721TransferTxHash) {
             //console.log("Transfer Hash", erc721TransferTxHash);
-            this.refreshNFTTokens();
+            setTimeout(() => {
+              this.refreshNFTTokens()
+            }, 7000);
+
             app.addToast(
               "Transferred successfully",
               "You successfully transferred the token",
@@ -419,7 +422,10 @@ export default class SendToken extends Vue {
               gasPrice: 1000000000,
             });
           if (erc1155TransferTxHash) {
-            this.refreshNFTTokens();
+            setTimeout(() => {
+              this.refreshNFTTokens()
+            }, 7000);
+
             app.addToast(
               "Transferred successfully",
               "You successfully transferred the token",
